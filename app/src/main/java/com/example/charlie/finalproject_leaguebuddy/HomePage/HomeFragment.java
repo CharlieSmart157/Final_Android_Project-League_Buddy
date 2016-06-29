f6a8376eacb863478e03ac2781b4376cf3728544
charliesmart157 <charliesmart157@gmail.com>
2016-06-29 10:26:24 +0100

    first commit

---
+++ app/src/main/java/com/example/charlie/finalproject_leaguebuddy/HomePage/HomeFragment.java
@@ -0,0 +1,11 @@
+package com.example.charlie.finalproject_leaguebuddy.HomePage;
+
+import com.example.charlie.finalproject_leaguebuddy.BaseTemplates.BaseFragment;
+
+/**
+ * Created by Charlie on 28/06/2016.
+ */
+public class HomeFragment extends BaseFragment {
+
+
+}
